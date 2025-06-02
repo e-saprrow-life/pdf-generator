b77e972d3fa3ed6b799c0b3055c2b020ef1e3d99
Yevhenii Vorobei <91190880+e-saprrow-life@users.noreply.github.com>
2025-06-02 11:02:12 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import puppeteer from 'puppeteer';
+
+console.log(process.env.PORT)
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.post('/generate-pdf', async  (req, res) => {
+    const data = req.body;
+    const url = data.url;
+    if (!url) res.status(400).send('No url');
+
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox'], // потрібні для продакшн-серверів
+    });
+
+    const page = await browser.newPage();
+    // await page.setContent(url, { waitUntil: 'domcontentloaded' });
+    await page.goto(url, { waitUntil: 'domcontentloaded' });
+
+    const pdfBuffer = await page.pdf({
+        format: 'A4',
+        printBackground: true,
+    });
+
+    await browser.close();
+
+    res.set({
+        'Content-Type': 'application/pdf',
+        'Content-Disposition': 'attachment; filename="generated.pdf"',
+    });
+   
+    res.send(pdfBuffer)
+})
+
+
+app.listen(PORT, () => console.log(`Run on port ${PORT}`) )
---
+++ package.json.b
@@ -0,0 +1,17 @@
+{
+  "name": "pdf-generator",
+  "version": "1.0.0",
+  "description": "generate pdf from web page",
+  "license": "ISC",
+  "author": "Yevhenii Vorobey",
+  "type": "commonjs",
+  "main": "index.js",
+  "scripts": {
+    "test": "npm run test",
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^5.1.0",
+    "puppeteer": "^24.9.0"
+  }
+}
